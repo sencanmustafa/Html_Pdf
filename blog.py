260bb5368dd988c9f1d56ffce78b9ab24d2914de
Mustafa <sencanmustafa54@gmail.com>
2022-04-16 22:05:23 +0300

    #Code improvement and some bug fixes

--- blog.py
+++ blog.py
@@ -1,20 +1,14 @@
 # -*- coding:utf-8 -*-
 import jinja2
+import pdfkit
 import wtforms
 from flask import Flask ,Response, render_template ,flash,redirect,url_for,session,logging,request,make_response
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
-import pdfcrowd
-import sys
-import pandas as pd
-from weasyprint import HTML
 import urllib.parse
 from passlib.hash import sha256_crypt
 from werkzeug.security import generate_password_hash,check_password_hash
 import data_acces
 from functools import wraps
-
-
-
 
 
 class LoginForm(Form):
@@ -35,7 +29,6 @@ def login_required(f):
         if "logged_in" in session:
             return f(*args, **kwargs)
         else:
-
             return redirect(url_for("login"))
     return decorated_function
 
@@ -56,20 +49,23 @@ def login():
     else:
         return render_template("login.html", form = form)
 
-
-
-
 @app.route("/get_pdf",methods = ["GET","POST"])
 @login_required
 def get_pdf():
-    pass
-
+    session['my_list'] = data_acces.liste
+    if request.method == "POST":
+        rendered = render_template('get_pdf.html')
+        pdf = pdfkit.from_string(rendered,False)
+        response = make_response(pdf)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-disposition'] = 'attachment ; filename = output.pdf'
+        return response
+    return redirect(url_for('converted'))
 
 @app.route("/converted",methods = ["GET","POST"])
 @login_required
 def converted():
-    pdf_name = 'pdf'
-    HTML(string = html.out).write_pdf(pdf_name)
+    session['my_list'] = data_acces.liste
     return render_template("converted.html")
 
 
@@ -80,6 +76,7 @@ def converted():
 
 @app.route("/logout")
 def logout():
+    data_acces.liste.clear()
     session.clear()
     return redirect(url_for("login"))
 
@@ -91,11 +88,11 @@ def convert():
     if request.method == "POST":
         data_text1 = html_form.text1.data
         data_text2 = html_form.text2.data
-        mylist  = [data_text1,data_text2]
-        # data in the list
+        data_acces.liste.append(data_text1)
+        data_acces.liste.append(data_text2)
         flash("Converted...","success")
-        session['my_list'] = mylist
-        return redirect(url_for("converted",mylist = mylist))
+        session['my_list'] = data_acces.liste
+        return redirect(url_for("converted",my_list = data_acces.liste))
     return render_template("convert.html",form = html_form)
 
 
@@ -109,7 +106,10 @@ def convert():
 
 @app.route("/")
 def index():
+    for i in data_acces.liste:
+        print(i)
     return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
+
 
